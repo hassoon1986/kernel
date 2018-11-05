d140af92e59acb6857f7dda2f8a62b6951ba2993
Oliver Neukum <oneukum@suse.com>
2018-11-05 14:16:32 +0100

    - USB: keyspan: fix tty line-status reporting (bsc#1114672).
    suse-commit: 2f7af285ff90823f89e75d51c1b4296fb4849184

--- drivers/usb/serial/keyspan.c
+++ drivers/usb/serial/keyspan.c
@@ -451,9 +451,9 @@ static void	usa26_indat_callback(struct urb *urb)
 				if (stat & RXERROR_OVERRUN)
 					flag |= TTY_OVERRUN;
 				if (stat & RXERROR_FRAMING)
-					flag |= TTY_FRAME;
-				if (stat & RXERROR_PARITY)
-					flag |= TTY_PARITY;
+					flag = TTY_FRAME;
+				else if (stat & RXERROR_PARITY)
+					flag = TTY_PARITY;
 				/* XXX should handle break (0x10) */
 				tty_insert_flip_char(tty, data[i+1], flag);
 			}
@@ -834,11 +834,11 @@ static void	usa49_indat_callback(struct urb *urb)
 			for (i = 0; i + 1 < urb->actual_length; i += 2) {
 				int stat = data[i], flag = 0;
 				if (stat & RXERROR_OVERRUN)
-					flag |= TTY_OVERRUN;
+					tty_insert_flip_char(&port->port, 0, TTY_OVERRUN);
 				if (stat & RXERROR_FRAMING)
-					flag |= TTY_FRAME;
-				if (stat & RXERROR_PARITY)
-					flag |= TTY_PARITY;
+					flag = TTY_FRAME;
+				else if (stat & RXERROR_PARITY)
+					flag = TTY_PARITY;
 				/* XXX should handle break (0x10) */
 				tty_insert_flip_char(tty, data[i+1], flag);
 			}
@@ -902,11 +902,11 @@ static void usa49wg_indat_callback(struct urb *urb)
 				for (x = 0; x + 1 < len; x += 2) {
 					int stat = data[i], flag = 0;
 					if (stat & RXERROR_OVERRUN)
-						flag |= TTY_OVERRUN;
+						tty_insert_flip_char(&port->port, 0, TTY_OVERRUN);
 					if (stat & RXERROR_FRAMING)
-						flag |= TTY_FRAME;
-					if (stat & RXERROR_PARITY)
-						flag |= TTY_PARITY;
+						flag = TTY_FRAME;
+					else if (stat & RXERROR_PARITY)
+						flag = TTY_PARITY;
 					/* XXX should handle break (0x10) */
 					tty_insert_flip_char(tty,
 							data[i+1], flag);
@@ -980,11 +980,11 @@ static void usa90_indat_callback(struct urb *urb)
 				for (i = 0; i + 1 < urb->actual_length; i += 2) {
 					int stat = data[i], flag = 0;
 					if (stat & RXERROR_OVERRUN)
-						flag |= TTY_OVERRUN;
+						tty_insert_flip_char(&port->port, 0, TTY_OVERRUN);
 					if (stat & RXERROR_FRAMING)
-						flag |= TTY_FRAME;
-					if (stat & RXERROR_PARITY)
-						flag |= TTY_PARITY;
+						flag = TTY_FRAME;
+					else if (stat & RXERROR_PARITY)
+						flag = TTY_PARITY;
 					/* XXX should handle break (0x10) */
 					tty_insert_flip_char(tty, data[i+1],
 									flag);
