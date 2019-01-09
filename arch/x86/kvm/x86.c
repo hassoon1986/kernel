4342e3eb14474626c78eafbdd06fa755318ca2b3
Joerg Roedel <jroedel@suse.de>
2019-01-09 16:44:38 +0100

    KVM: X86: Fix scan ioapic use-before-initialization (CVE-2018-19407 bsc#1116841).
    suse-commit: 19bbc5de7de30de342ff0eb0b5216159bc2e9ef7

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -564,6 +564,7 @@ static void kvm_put_guest_xcr0(struct kvm_vcpu *vcpu)
 
 static void vcpu_scan_ioapic(struct kvm_vcpu *vcpu)
 {
+	struct kvm *kvm = vcpu->kvm;
 	u64 eoi_exit_bitmap[4];
 	u32 tmr[8];
 
@@ -573,7 +574,8 @@ static void vcpu_scan_ioapic(struct kvm_vcpu *vcpu)
 	memset(eoi_exit_bitmap, 0, 32);
 	memset(tmr, 0, 32);
 
-	kvm_ioapic_scan_entry(vcpu, eoi_exit_bitmap, tmr);
+	if (kvm->arch.vioapic != NULL)
+		kvm_ioapic_scan_entry(vcpu, eoi_exit_bitmap, tmr);
 	kvm_x86_ops->load_eoi_exitmap(vcpu, eoi_exit_bitmap);
 	kvm_apic_update_tmr(vcpu, tmr);
 }
