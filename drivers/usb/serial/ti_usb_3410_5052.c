b675b811c6c7d25a633306ea7683884c1e4e0ee6
Oliver Neukum <oneukum@suse.com>
2019-01-31 11:01:43 +0100

    USB: serial: ti_usb_3410_5052: fix NULL-deref at open (bsc#1114672).
    suse-commit: f3bda01b1d106a95d3b0bf560d1bd4145517631f

--- drivers/usb/serial/ti_usb_3410_5052.c
+++ drivers/usb/serial/ti_usb_3410_5052.c
@@ -463,6 +463,13 @@ static int ti_startup(struct usb_serial *serial)
 		tport->tp_uart_mode = 0;	/* default is RS232 */
 	}
 
+	if (serial->num_bulk_in < serial->num_ports ||
+			serial->num_bulk_out < serial->num_ports) {
+		dev_err(&serial->interface->dev, "missing endpoints\n");
+		status = -ENODEV;
+		goto free_tdev;
+	}
+
 	return 0;
 
 free_tports:
