2461341b4b6246911213280d53a4717610e4ac1e
Denis Kirjanov <dkirjanov@suse.com>
2019-02-28 12:53:04 +0100

    net/mlx5: Fix use-after-free in self-healing flow (git-fixes).
    suse-commit: a56f15d5e3cc7a98dc9036fdc2dda4dd99da0ced

--- include/linux/mlx5/driver.h
+++ include/linux/mlx5/driver.h
@@ -928,7 +928,7 @@ int mlx5_cmd_free_uar(struct mlx5_core_dev *dev, u32 uarn);
 void mlx5_health_cleanup(struct mlx5_core_dev *dev);
 int mlx5_health_init(struct mlx5_core_dev *dev);
 void mlx5_start_health_poll(struct mlx5_core_dev *dev);
-void mlx5_stop_health_poll(struct mlx5_core_dev *dev);
+void mlx5_stop_health_poll(struct mlx5_core_dev *dev, bool disable_health);
 void mlx5_drain_health_wq(struct mlx5_core_dev *dev);
 void mlx5_drain_health_recovery(struct mlx5_core_dev *dev);
 int mlx5_buf_alloc_node(struct mlx5_core_dev *dev, int size,
